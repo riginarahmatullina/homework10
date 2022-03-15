358737be52429f8615ca2db109c46de98368bfa1
riginarahmatullina <riginarahmattullina@yandex.ru>
2022-03-15 18:52:45 +0300

    init

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+import utils
+
+app = Flask(__name__)
+
+candidates = utils.load_candidates()
+
+
+@app.route("/")
+def page_index():
+    str_candidates = '<pre>'
+    for candidate in candidates.values():
+        str_candidates += f"{candidate['name']}\n{candidate['position']}\n{candidate['skills']}\n\n"
+    str_candidates += '<pre>'
+    return str_candidates
+
+
+@app.route("/candidates/<int:id>")
+def profile(id):
+    candidate = candidates[id]
+
+    str_candidates = f"<img src={candidate['picture']}></img> <br><br>{candidate['name']} <br>{candidate['position']} <br>{candidate['skills']} <br><br>"
+
+    return str_candidates
+
+@app.route("/skills/<skill>")
+def skills(skill):
+    str_candidates = ''
+
+    for candidate in candidates.values():
+        candidate_skills = candidate['skills'].split(', ')
+        candidate_skills = [x.lower() for x in candidate_skills]
+        if skill in candidate_skills:
+            str_candidates += f"{candidate['name']} <br>{candidate['position']} <br>{candidate['skills']} <br><br>"
+        str_candidates += ""
+
+        return str_candidates
+
+
+app.run()
